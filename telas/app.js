b480c8dd0f5053575f0d400ef8d41466b9b1c952
Marcella <129318421+marcella28@users.noreply.github.com>
2023-12-04 17:37:30 -0300

    + alt pra exibir o ranking que não deram certo

--- telas/app.js
+++ telas/app.js
@@ -1,72 +1,72 @@
-// Configuração do servidor (adapte conforme necessário)
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const mysql = require('mysql');
+const fs = require('fs');
+const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
 const port = 5500;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/telas'));
 
-// Configuração da session
-app.use(session({
-  secret: 'sua_chave_secreta',
-  resave: true,
-  saveUninitialized: true
-}));
+// Rota /gerarTabela
+app.route('/gerarTabela')
+  .get((req, res) => {
+    // Se for uma solicitação GET, renderiza a página do formulário
+    res.sendFile(__dirname + '/telas/index.html');
+  })
+  .post((req, res) => {
+    // Se for uma solicitação POST, processa o formulário
+    const { ensino, anosEscolares, ano } = req.body;
 
-// Configuração do banco de dados (adapte conforme necessário)
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'acesso123',
-  database: 'saeb',
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
-  } else {
-    console.log('Conectado ao banco de dados MySQL');
-  }
-});
-
-// Rota /gerarTabela (adapte conforme necessário)
-app.post('/gerarTabela', (req, res) => {
-  const { ensino, anosEscolares, ano } = req.body;
-
-  if (!ensino || !anosEscolares || !ano) {
-    return res.status(400).send('Parâmetros inválidos');
-  }
-
-  let query = `
-    SELECT ref.nome AS nome_instituicao, dados.ideb
-    FROM dados_escolares AS dados
-    INNER JOIN ref_escolas AS ref ON dados.id_escola = ref.id_escola
-    WHERE dados.ensino = ? AND dados.anos_escolares = ? AND dados.ano = ?
-    ORDER BY dados.ideb DESC;
-  `;
-
-  db.query(query, [ensino, anosEscolares, ano], (err, results) => {
-    if (err) {
-      console.error('Erro ao consultar o banco de dados:', err);
-      return res.status(500).send('Erro ao processar a solicitação');
+    if (!ensino || !anosEscolares || !ano) {
+      console.error('Parâmetros inválidos');
+      return res.status(400).send('Parâmetros inválidos');
     }
 
+    // Alteração para ler dados do arquivo dadosIdeb.json
+    const filePathIdeb = path.join(__dirname, 'dadosIdeb.json');
+    const dataIdeb = JSON.parse(fs.readFileSync(filePathIdeb, 'utf8'));
+
+    // Filtre os dados com base nos parâmetros recebidos
+    const filteredDataIdeb = dataIdeb.filter(item => (
+      item.ensino === ensino &&
+      item.anos_escolares === anosEscolares &&
+      item.ano === ano
+    ));
+
+    // Alteração para ler dados do arquivo dadosEscolas.json
+    const filePathEscolas = path.join(__dirname, 'dadosEscolas.json');
+    const dataEscolas = JSON.parse(fs.readFileSync(filePathEscolas, 'utf8'));
+
+    // Mapeia o ID da escola para o nome da escola
+    const idToNome = dataEscolas.reduce((acc, item) => {
+      acc[item.id_escola] = item.nome;
+      return acc;
+    }, {});
+
+    // Adiciona o nome da escola aos dados do IDEB
+    const dataFinal = filteredDataIdeb.map(item => ({
+      ...item,
+      nome_instituicao: idToNome[item.id_escola]
+    }));
+
     // Armazena os dados na session storage
-    req.session.resultadoData = JSON.stringify(results);
+    req.session.resultadoData = JSON.stringify(dataFinal);
 
     // Redireciona para a página de resultado
     res.redirect('/resultado');
   });
-});
 
 // Rota /resultado
 app.get('/resultado', (req, res) => {
-  // Renderiza a página 'resultado'
-  res.sendFile(__dirname + '/telas/resultado.html');
+  // Recupera os dados armazenados na session storage
+  const data = JSON.parse(req.session.resultadoData || '[]');
+
+  // Renderiza a página EJS
+  res.render('telaResultado', { rows: data });
 });
 
 // Inicie o servidor
